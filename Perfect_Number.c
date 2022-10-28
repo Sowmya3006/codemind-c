d87c265915100335c2f4d95824cb6907bea70918
Sowmya3006 <89642540+Sowmya3006@users.noreply.github.com>
2022-10-28 10:20:43 +0530

    #include<stdio.h> int main(){     int n,c=0;     scanf("%d",&n);     for(int i=1;i<n;i++){         if(n%i==0){             c+=i;         }
        }
        if(n==c)
        printf("True");
        else
        printf("False");
    }

---
+++ Perfect_Number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,c=0;
+    scanf("%d",&n);
+    for(int i=1;i<n;i++){
+        if(n%i==0){
+            c+=i;
+        }
+        
+    }
+    if(n==c)
+    printf("True");
+    else
+    printf("False");
+}
